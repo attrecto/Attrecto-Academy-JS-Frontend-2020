a9eb5f901685d26692f4ef3ac9e31cb745718a43
mate_illes <mate.illes@attrecto.com>
2020-02-27 13:45:48 +0100

    4 - Create first api call, get badges

--- src/components/badges/Badges.js
+++ src/components/badges/Badges.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import { getBadges } from "../../services/badges";
+import Button from "../button/Button";
 
-const Badges = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">Badges</div>
+class Badges extends Component {
+  state = {
+    badges: []
+  };
+
+  async componentDidMount() {
+    const badges = await getBadges();
+    this.setState({ badges });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div>Badges</div>
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>{""}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.badges.map((badge, index) => (
+                  <tr key={index}>
+                    <td>{index}</td>
+                    <td>{badge.name}</td>
+                    <td>{badge.description}</td>
+                    <td>
+                      <Button>Remove</Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Badges;
