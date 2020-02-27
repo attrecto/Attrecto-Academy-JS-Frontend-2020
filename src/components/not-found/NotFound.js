c594e2ef163671bd8472462dac39288f4f0eeb5c
mate_illes <mate.illes@attrecto.com>
2020-02-27 13:28:39 +0100

    3 - Install react router, create badges, home,  users component

---
+++ src/components/not-found/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <strong>404</strong> page not found!
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
