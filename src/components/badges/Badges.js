5ea39e103b9b162e3608f8c2cd6bd5d030f63183
mate_illes <mate.illes@attrecto.com>
2020-02-28 10:08:16 +0100

    4 - Create Badges component with hook

--- src/components/badges/Badges.js
+++ src/components/badges/Badges.js
@@ -1,50 +1,49 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { getBadges } from "../../services/badges";
 import Button from "../button/Button";
 
-class Badges extends Component {
-  state = {
-    badges: []
-  };
+const Badges = () => {
+  const [badges, setBadges] = useState([]);
 
-  async componentDidMount() {
-    const badges = await getBadges();
-    this.setState({ badges });
-  }
+  useEffect(() => {
+    async function fetchData() {
+      const badges = await getBadges();
+      setBadges(badges);
+    }
+    fetchData();
+  }, []);
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div>Badges</div>
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Name</th>
-                  <th>Description</th>
-                  <th>{""}</th>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div>Badges</div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>{""}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {badges.map((badge, index) => (
+                <tr key={index}>
+                  <td>{index}</td>
+                  <td>{badge.name}</td>
+                  <td>{badge.description}</td>
+                  <td>
+                    <Button>Remove</Button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {this.state.badges.map((badge, index) => (
-                  <tr key={index}>
-                    <td>{index}</td>
-                    <td>{badge.name}</td>
-                    <td>{badge.description}</td>
-                    <td>
-                      <Button>Remove</Button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Badges;
